ae20c64c610cb23a4512a89c4bc07098cc38a700
wilckerson <wilckerson@gmail.com>
2015-06-09 14:52:27 -0300

    Migracao android studio

--- android_studio/app/src/main/java/com/wilckersonganda/cameradraw/MainActivity.java
+++ android_studio/app/src/main/java/com/wilckersonganda/cameradraw/MainActivity.java
@@ -1,17 +1,44 @@
 package com.wilckersonganda.cameradraw;
 
+import android.hardware.Camera;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import java.io.IOException;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements SurfaceHolder.Callback {
+
+    private SurfaceView surfaceView;
+    private SurfaceHolder surfaceHolder;
+    private Camera mCamera;
+
+    public static Camera getCameraInstance() {
+        Camera c = null;
+        try {
+            c = Camera.open();
+        } catch (Exception e) {
+            Log.d("MainActivity", "Could not open camera");
+        }
+        return c;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            surfaceView = (SurfaceView) findViewById(R.id.surfaceview);
+            surfaceHolder = surfaceView.getHolder();
+            surfaceHolder.addCallback(this);
+            surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+        }
     }
 
     @Override
@@ -34,5 +61,29 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        try {
+            mCamera = Camera.open();
+            mCamera.setDisplayOrientation(90);
+
+            mCamera.setPreviewDisplay(surfaceHolder);
+            mCamera.startPreview();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+
     }
 }
